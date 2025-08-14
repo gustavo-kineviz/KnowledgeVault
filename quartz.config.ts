707554b608d1911418be2ca6e116e219da05bccf
Gustavo <gustavo@kineviz.com>
2025-08-13 23:42:00 -0300

    Quartz sync: Aug 13, 2025, 11:42 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "gustavo-kineviz.github.io/KnowledgeVault",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
